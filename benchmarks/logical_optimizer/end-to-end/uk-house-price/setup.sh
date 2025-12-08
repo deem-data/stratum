596ee859b549c1741c6adead0a9e7886469ad603
e-strauss <elias.strauss@tu-berlin.de>
2025-12-08 15:56:41 +0100

    new exp

---
+++ benchmarks/logical_optimizer/end-to-end/uk-house-price/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download dataset from kaggle
+curl -L -o ~/Downloads/uk-housing-prices-paid.zip\
+  https://www.kaggle.com/api/v1/datasets/download/hm-land-registry/uk-housing-prices-paid
+
+unzip uk-housing-prices-paid.zip -d tmp
+mkdir -p input
+mv tmp/* input/
+rm -rf tmp
+rm uk-housing-prices-paid.zip
+
+# downsample for testing:
+head -100000 input/price_paid_records.csv > input/price_paid_records_small.csv
