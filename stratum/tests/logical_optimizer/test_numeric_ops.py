8a40ee65065080954900821a65d08d2c702da217
Tim Schuster <tim13187@gmail.com>
2026-05-07 14:18:13 +0200

    Add more tests

--- stratum/tests/logical_optimizer/test_numeric_ops.py
+++ stratum/tests/logical_optimizer/test_numeric_ops.py
@@ -24,6 +24,41 @@ class TestNumericOps(unittest.TestCase):
         with st.config(scheduler=True):
             pred.skb.make_grid_search(cv=3)
 
+    def test_to_numeric_op_abs(self):
+        data = st.as_data_op(self.df)
+        X = data[["x"]].skb.mark_as_X()
+        y = data["y"].skb.mark_as_y()
+        t1 = X.skb.apply_func(np.abs)
+        pred = t1.skb.apply(DummyRegressor(), y=y)
+
+        with st.config(scheduler=True):
+            pred.skb.make_grid_search(cv=3)
+
+    def test_process_log(self):
+        op = NumericOp(inputs=[], outputs=None, func=np.log)
+        result = op.process("fit", {}, [np.array([1.0, np.e, np.e**2])])
+        np.testing.assert_array_almost_equal(result, np.array([0.0, 1.0, 2.0]))
+
+    def test_process_exp(self):
+        op = NumericOp(inputs=[], outputs=None, func=np.exp)
+        result = op.process("fit", {}, [np.array([0.0, 1.0, 2.0])])
+        np.testing.assert_array_almost_equal(result, np.array([1.0, np.e, np.e**2]))
+
+    def test_process_sqrt(self):
+        op = NumericOp(inputs=[], outputs=None, func=np.sqrt)
+        result = op.process("fit", {}, [np.array([4.0, 9.0, 16.0])])
+        np.testing.assert_array_almost_equal(result, np.array([2.0, 3.0, 4.0]))
+
+    def test_process_abs(self):
+        op = NumericOp(inputs=[], outputs=None, func=np.abs)
+        result = op.process("fit", {}, [np.array([-3.0, 0.0, 5.0])])
+        np.testing.assert_array_almost_equal(result, np.array([3.0, 0.0, 5.0]))
+
+    def test_process_square(self):
+        op = NumericOp(inputs=[], outputs=None, func=np.square)
+        result = op.process("fit", {}, [np.array([2.0, 3.0, 4.0])])
+        np.testing.assert_array_almost_equal(result, np.array([4.0, 9.0, 16.0]))
+
     def test_unsupported_numeric_op(self):
         op = NumericOp(inputs=[], outputs=None, func=np.cos)
         op.type = "unsupported"
